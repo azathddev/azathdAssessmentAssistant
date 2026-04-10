7e1528dd65760c95d4722bbd85e7a6e999bcc81f
azathd <azathd@yandex.ru>
2026-04-10 14:03:08 +0300

    some repairs

--- server/settings/components/common.py
+++ server/settings/components/common.py
@@ -22,7 +22,7 @@ SECRET_KEY = config('DJANGO_SECRET_KEY')
 INSTALLED_APPS: tuple[str, ...] = (
     # Your apps go here:
     'server.apps.main',
-    'server.apps.auth'
+    'server.apps.auth',
     # Default django apps:
     'django.contrib.auth',
     'django.contrib.contenttypes',
